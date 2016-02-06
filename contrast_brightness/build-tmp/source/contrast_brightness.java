45d93c26a5378f20f2365ae33598e31ae5bc7497
Omar V <omar.vazquezac@gmail.com>
2016-02-06 00:13:13 -0600

    brightness and contrast working

--- contrast_brightness/build-tmp/source/contrast_brightness.java
+++ contrast_brightness/build-tmp/source/contrast_brightness.java
@@ -18,29 +18,41 @@ int B_MASK = 255;
 int G_MASK = 255<<8;
 int R_MASK = 255<<16;
 
+//String 
+
 class Colors {
-	float new_red, new_green, new_blue;
+	float contrast_red, contrast_green, contrast_blue;
+	float brightness_red, brightness_green, brightness_blue;
 	int red, green, blue;
 
 	public Colors (int info) {
-		//info *= -1;
-		//println(rand);
-		int r, g, b;
+		//int r, g, b;
 		red = (info & R_MASK)>>16;
 		green = (info & G_MASK)>>8;
 		blue = info & B_MASK;
 
-		new_red = (red*rand);
-		new_green = (green*rand);
-		new_blue = (blue*rand);
+		contrast_red = contrastCalculation(red);
+		contrast_green = contrastCalculation(green);
+		contrast_blue = contrastCalculation(blue);
+
+		brightness_red = brightnessCalculator(red);
+		brightness_green = brightnessCalculator(green);
+		brightness_blue = brightnessCalculator(blue);
 	}
 
+	public float contrastCalculation(int color_value) {
+		return color_value * contrast_rand;
+	}
+
+	public float brightnessCalculator(int color_value) {
+		return color_value + brightness_rand;
+	}
 }
 
 Colors[] colors;
 PImage img;
 boolean floppy = true;
-float rand;
+float contrast_rand, brightness_rand;
 int r,g,b;
 int[] pix;
 
@@ -48,8 +60,9 @@ public void setup() {
 	
 	img = loadImage("image_example.JPG");
 	colors = new Colors[img.width*img.height];
-	rand = random(1.1f, 2);
-	println(rand);
+	contrast_rand = random(1.1f, 2);
+	brightness_rand = random(1, 100);
+	println(contrast_rand + "    " + brightness_rand);
 	doResize();
 	image(img, 0, 0);
 	surface.setResizable(true);
@@ -57,23 +70,34 @@ public void setup() {
 }
 
 public void draw() {
-	if(floppy) {
-		image(img, 0, 0);
-		floppy = !floppy;
-		save("data/contrast.jpg");
-	}
+	image(img, 0, 0);
+	change_contrast_or_brightness("contrast");
+	img = loadImage("image_example.JPG");
+	image(img, 0, 0);
+	change_contrast_or_brightness("brightness");
+	//save("data/brightness.jpg");
+	exit();
 }
 
 public void doResize() {
 	surface.setSize(img.width, img.height);
+}
+
+public void change_contrast_or_brightness(String method) {
 	img.loadPixels();
-	for (int i = 0; i < img.pixels.length; i++) {
-		colors[i] = new Colors(img.pixels[i]);
-		img.pixels[i] = color(colors[i].new_red, colors[i].new_green, colors[i].new_blue);
-		//pix[i] = color((int)(random(pix[i]%255)*(pix[i]%255)));
-		//println(colors[i].new_red + "   " + colors[i].new_green + "   " + colors[i].new_blue);
+	for (int i = 0; i < img.pixels.length; i++) colors[i] = new Colors(img.pixels[i]);
+	if(method.equals("contrast")) {
+		println("contrast");
+		for (int i = 0; i < img.pixels.length; i++) 
+			img.pixels[i] = color(colors[i].contrast_red, colors[i].contrast_green, colors[i].contrast_blue);
 	}
-	updatePixels();
+	else if(method.equals("brightness")) {
+		println("brightness");
+		for (int i = 0; i < img.pixels.length; i++) 
+			img.pixels[i] = color(colors[i].brightness_red, colors[i].brightness_green, colors[i].brightness_blue);
+	}
+	img.updatePixels();
+	save("data/" + method + ".jpg");
 }
   public void settings() { 	size(100, 100, P2D); }
   static public void main(String[] passedArgs) {
